9859e9681f8df0ebb35d4bf01db4c365af14398d
yakoub <yakoubtouati5@gmail.com>
2023-09-28 01:19:34 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let dark=document.querySelector("input[type=checkbox]");
+let all=document.querySelector(".tous")
+let header=document.querySelector(".tete")
+
+dark.addEventListener("click",()=>{
+    all.classList.toggle("all")
+    header.classList.toggle("header")
+    
+
+})
+  
+
+
+
+
+
+
